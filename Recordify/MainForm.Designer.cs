1d51c6eb03873c4f46769e19f36b75eb1968fd62
mdaugustine <michael.daugustine@gmail.com>
2015-06-22 11:33:54 -0500

    recordify

---
+++ Recordify/MainForm.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Recordify
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnRecord = new System.Windows.Forms.ToolStripButton();
+            this.btnStop = new System.Windows.Forms.ToolStripButton();
+            this.btnPause = new System.Windows.Forms.ToolStripButton();
+            this.btnPlay = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnSaveUncompressed = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnSaveCompressed = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.recordingWoker = new System.ComponentModel.BackgroundWorker();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.previewBox = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.previewBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(697, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnRecord,
+            this.btnStop,
+            this.btnPause,
+            this.btnPlay,
+            this.toolStripSeparator1,
+            this.btnSaveUncompressed,
+            this.toolStripSeparator2,
+            this.btnSaveCompressed,
+            this.toolStripSeparator3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(697, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnRecord
+            // 
+            this.btnRecord.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnRecord.Image = ((System.Drawing.Image)(resources.GetObject("btnRecord.Image")));
+            this.btnRecord.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnRecord.Name = "btnRecord";
+            this.btnRecord.Size = new System.Drawing.Size(23, 22);
+            this.btnRecord.Text = "toolStripButton1";
+            this.btnRecord.Click += new System.EventHandler(this.btnRecord_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnStop.Image = ((System.Drawing.Image)(resources.GetObject("btnStop.Image")));
+            this.btnStop.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(23, 22);
+            this.btnStop.Text = "toolStripButton2";
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnPause.Image = ((System.Drawing.Image)(resources.GetObject("btnPause.Image")));
+            this.btnPause.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(23, 22);
+            this.btnPause.Text = "toolStripButton5";
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnPlay.Image = ((System.Drawing.Image)(resources.GetObject("btnPlay.Image")));
+            this.btnPlay.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(23, 22);
+            this.btnPlay.Text = "toolStripButton3";
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnSaveUncompressed
+            // 
+            this.btnSaveUncompressed.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.btnSaveUncompressed.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnSaveUncompressed.Image = ((System.Drawing.Image)(resources.GetObject("btnSaveUncompressed.Image")));
+            this.btnSaveUncompressed.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSaveUncompressed.Name = "btnSaveUncompressed";
+            this.btnSaveUncompressed.Size = new System.Drawing.Size(117, 22);
+            this.btnSaveUncompressed.Text = "Save Uncompressed";
+            this.btnSaveUncompressed.Click += new System.EventHandler(this.btnSaveUncompressed_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnSaveCompressed
+            // 
+            this.btnSaveCompressed.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.btnSaveCompressed.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnSaveCompressed.Image = ((System.Drawing.Image)(resources.GetObject("btnSaveCompressed.Image")));
+            this.btnSaveCompressed.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSaveCompressed.Name = "btnSaveCompressed";
+            this.btnSaveCompressed.Size = new System.Drawing.Size(114, 22);
+            this.btnSaveCompressed.Text = "Compress and Save";
+            this.btnSaveCompressed.Click += new System.EventHandler(this.btnSaveCompressed_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+            // 
+            // recordingWoker
+            // 
+            this.recordingWoker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.recordingWoker_DoWork);
+            this.recordingWoker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.recordingWoker_ProgressChanged);
+            this.recordingWoker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.recordingWoker_RunWorkerCompleted);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 476);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(697, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // previewBox
+            // 
+            this.previewBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.previewBox.Location = new System.Drawing.Point(0, 49);
+            this.previewBox.Name = "previewBox";
+            this.previewBox.Size = new System.Drawing.Size(697, 427);
+            this.previewBox.TabIndex = 3;
+            this.previewBox.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 498);
+            this.Controls.Add(this.previewBox);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Recordify";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.previewBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnRecord;
+        private System.Windows.Forms.ToolStripButton btnStop;
+        private System.Windows.Forms.ToolStripButton btnPause;
+        private System.Windows.Forms.ToolStripButton btnPlay;
+        private System.Windows.Forms.ToolStripButton btnSaveCompressed;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton btnSaveUncompressed;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.ComponentModel.BackgroundWorker recordingWoker;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.PictureBox previewBox;
+    }
+}
+
---
+++ Recordify/MainForm.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using AviFile;
+
+namespace Recordify
+{
+    enum Status
+    {
+        Stopped,
+        Paused,
+        Recording,
+        Playback
+    };
+
+    public partial class MainForm : Form
+    {
+        Boolean recording;
+        Status status;
+        List<Bitmap> frames;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            recordingWoker.WorkerReportsProgress = true;
+            recordingWoker.WorkerSupportsCancellation = true;
+            frames = new List<Bitmap>();
+            btnStop.Enabled = false;
+            btnPause.Enabled = false;
+            btnPlay.Enabled = false;
+        }
+
+        private void btnRecord_Click(object sender, EventArgs e)
+        {
+            //bitmaps.Clear();
+            frames.Clear();
+            status = Status.Recording;
+            recording = true;
+            btnRecord.Enabled = false;
+            btnStop.Enabled = true;
+            btnPlay.Enabled = false;
+            //btnPause.Enabled = true;
+
+            recordingWoker.RunWorkerAsync();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            status = Status.Stopped;
+            recording = false;
+            btnRecord.Enabled = true;
+            btnStop.Enabled = false;
+            btnPause.Enabled = false;
+            btnPlay.Enabled = true;
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            status = Status.Paused;
+            recording = false;
+            btnRecord.Enabled = true;
+            btnStop.Enabled = false;
+            btnPause.Enabled = false;
+            btnPlay.Enabled = true;
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            status = Status.Playback;
+            statusLabel.Text = status.ToString();
+            statusStrip1.Refresh();
+
+            if (frames.Count > 0)
+            {
+                for (int i = 0; i < frames.Count; i++)
+                {
+                    previewBox.Image = frames[i];
+                    previewBox.Refresh();
+                    statusLabel.Text = "Playing Frame " + i;
+                    statusStrip1.Refresh();
+                    System.Threading.Thread.Sleep(40);
+                }
+            }
+        }
+
+        private void recordingWoker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            statusLabel.Text = status.ToString();
+            //statusStrip1.Refresh();
+
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            /*
+            List<Bitmap> bitmaps = new List<Bitmap>();
+
+            using (Bitmap bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                Screen.PrimaryScreen.Bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bmpScreenshot))
+                {
+                    while(recording)
+                    {
+                        g.CopyFromScreen(0, 0, 0, 0, Screen.PrimaryScreen.Bounds.Size);
+                        bitmaps.Add(bmpScreenshot);
+                        previewBox.Image = bmpScreenshot;
+                    }
+                }
+            }
+
+            e.Result = bitmaps;
+            */
+
+            Bitmap bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics g = Graphics.FromImage(bmpScreenshot);
+            g.CopyFromScreen(0, 0, 0, 0, Screen.PrimaryScreen.Bounds.Size);
+
+            List<Bitmap> bitmaps = new List<Bitmap>();
+
+            while(recording)
+            {
+                bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+                g = Graphics.FromImage(bmpScreenshot);
+                g.CopyFromScreen(0, 0, 0, 0, Screen.PrimaryScreen.Bounds.Size);
+                bitmaps.Add(bmpScreenshot);
+                previewBox.Image = bmpScreenshot;
+                //previewBox.Refresh();
+            }
+
+            e.Result = bitmaps;
+
+            /*
+            AviManager aviManager = new AviManager(@"..\..\testdata\new.avi", false);
+            VideoStream aviStream = aviManager.AddVideoStream(true, 25, bitmaps[0]);
+
+            for (int i = 1; i < bitmaps.Count; i++)
+            {
+                aviStream.AddFrame(bitmaps[i]);
+                statusLabel.Text = "Compressing: " + i + "/" + (bitmaps.Count - 1);
+            }
+
+            aviManager.Close();
+            */
+        }
+
+        private void recordingWoker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //frames = frames.Concat((List<Bitmap>)e.Result).ToList();
+            frames = (List<Bitmap>)e.Result;
+
+            statusLabel.Text = status.ToString();
+            statusStrip1.Refresh();
+        }
+
+        private void btnSaveCompressed_Click(object sender, EventArgs e)
+        {
+            AviManager aviManager = new AviManager(@"..\..\testdata\new.avi", false);
+            VideoStream aviStream = aviManager.AddVideoStream(true, 25, frames[0]);
+
+            for (int i = 1; i < frames.Count; i++)
+            {
+                aviStream.AddFrame(frames[i]);
+                statusLabel.Text = "Compressing: " + i + "/" + (frames.Count - 1);
+                statusStrip1.Refresh();
+            }
+
+            aviManager.Close();
+
+            frames.Clear();
+            status = Status.Stopped;
+        }
+
+        private void btnSaveUncompressed_Click(object sender, EventArgs e)
+        {
+            AviManager aviManager = new AviManager(@"..\..\testdata\new.avi", false);
+            VideoStream aviStream = aviManager.AddVideoStream(false, 25, frames[0]);
+
+            for (int i = 1; i < frames.Count; i++)
+            {
+                aviStream.AddFrame(frames[i]);
+                statusLabel.Text = "Compressing: " + i + "/" + (frames.Count - 1);
+                statusStrip1.Refresh();
+            }
+
+            aviManager.Close();
+
+            frames.Clear();
+            status = Status.Stopped;
+        }
+
+        private void recordingWoker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+        }
+    }
+}
